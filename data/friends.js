d4b36e388210d38dbb383c2c554d65f8947c274a
DENIS PLASTER <denjeo@gmail.com>
2018-10-25 22:59:04 -0500

    final, before readme

---
+++ data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Zack Morris",
+		photo: "https://media.giphy.com/media/IrAkpeVcnY1YA/giphy.gif",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "A.C. Slater",
+		photo: "https://media.giphy.com/media/doT44h6AzH4DC/giphy.gif",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Kelly Kapowski",
+		photo: "https://media.giphy.com/media/RL34CvIMckFQA/giphy.gif",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Screech",
+		photo: "https://media.giphy.com/media/F5NxpsM2gRGWQ/giphy.gif",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Jessie",
+		photo: "https://media.giphy.com/media/cQN9El2Yge17W/giphy.gif",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
